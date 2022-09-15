3ed07e788617a5967ad2c1db49a8b3d825d03deb
Ben Mueller <benjm2002@gmail.com>
2022-09-15 14:31:37 -0500

    correctly opens page and gets all links

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+from os import link
 import selenium
 import json
 import sys
@@ -40,11 +41,36 @@ def main():
     if len(accept_cookies_btn) > 0:  # find_elements will return empty list if button not found
         accept_cookies_btn[0].click()
 
+    # open rest of page
+    sleep(2.5)
+    driver.find_element(
+        By.XPATH, 
+        "/html/body/div[@class='site-container']/div[@id='maincontent']/div[@class='row-fluid']/div[@id='content']/div[@class='span12']/div[@class='search mw-search-page']/div[@class='row-fluid']/div[@class='span9']/div[@class='pagination']/div[@class='btn btn-ncabrown load-more']"
+    ).click()
+    
+    
+    # get list of all divs containing criminals
+    criminal_divs = driver.find_elements(
+        By.CLASS_NAME,
+        "span4"
+    )
+
+    # get links to each criminal's page
+    criminal_links = []
+    for div in criminal_divs:
+        l = div.find_element(By.TAG_NAME, "a")
+        criminal_links.append(l)
+    
+    print(criminal_divs)
+    print(criminal_links)
+
+    # scrape and record data from each criminals page
+    
+
+
+
     # close driver
     driver.close()
-
-
-
 
 
 
